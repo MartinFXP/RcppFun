f0f1cfbe6c238f8383be9ac7c14d6baa204a8727
mpirkl <martinpirkl@yahoo.de>
2022-07-07 15:03:03 +0200

    now works automated with roxygen2

---
+++ R/dummy.r
@@ -0,0 +1,8 @@
+#' Dummy function
+#'
+#' Returns NULL and is used to auto import other packages
+#' @return NULL
+#' @export
+#' @import Rcpp RcppEigen
+#' @useDynLib RcppFun
+dummyfunction <- function() NULL
